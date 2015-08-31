865302a538c7d841795d9fde8d6c1084286c97d6
bleucanary <cowens@coroware.com>
2015-08-31 14:48:28 -0500

    Extending Script after testing

--- setup.bash
+++ setup.bash
@@ -37,7 +37,7 @@ CoroBot CUBIEEZ Setup Tool Information
 ===============================================
 
 The following software packages will be installed 
-$COL_GREEN
+
 * OpenCV
 * ROS
 * Python Developer Tools (Cython, HIDAPI, etc)
@@ -55,3 +55,54 @@ read ans
 	echo -n -e "
 	$COL_GREEN
 The CoroBot Cubieez setup tool will begin the configuration process"
+echo $COL_CYAN"Running Software Updates and Upgrades"
+$COL_WHITE
+sudo apt-get udpate && sudo apt-get upgrade -y 
+
+echo $COL_CYAN "Creating a Downlaods Directory"
+$COL_WHITE
+mkdir ~/Downloads
+ls ~/
+echo $COL_CYAN "Installing Preliminary Tools and Packages"
+$COL_WHITE
+sudo apt-get install build-essential vim emacs screen checkinstall subversion python-pip ipython3 ipython3-notebook python-zmq
+clear
+
+echo $COL_CYAN "Installing Libsodioum as a pre-req for ZMQ"
+$COL_WHITE
+cd ~/Downloads
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar -zxvf LATEST.tar.gz
+cd libsodium*
+./configure
+make && sudo make check
+sudo make install
+
+echo $COL_CYAN "Getting ZMQ Messaging Tools"
+$COL_WHITE
+cd ~/Downloads
+wget http://download.zeromq.org/zeromq-4.1.3.tar.gz
+tar -zxvf zeromq-4.1.3.tar.gz
+cd zeromq-4.1.3
+./configure
+make && sudo make check
+sudo make install
+sudo ldconfig
+echo $COL_RED "Exiting Downloads Folder"
+cd ~/
+clear
+
+echo $COL_CYAN "Installing Python related tools and dependencies"
+$COL_WHITE
+sudo apt-get install python3 python3-pyside python3-all-dev python3-zmq libzmq-dev python3-dev libusb-1.0-0-dev libudev-dev python3-dev -y
+clear
+echo $COL_CYAN "Getting PIP; the Python install manager"
+$COL_WHITE
+cd ~/Downloads
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+sudo pip3 install numpy virtualenv virtualenvwrapper pyzmq cython
+sudo pip3 install hidapi
+echo $COL_CYAN "Installing OpenCV 3.0.0"
+$COL_WHITE
+
