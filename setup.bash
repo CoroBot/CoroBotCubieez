73b9555fd09859bfe326827f577e1aa82221ece9
bleucanary <cowens@coroware.com>
2015-09-01 14:50:16 -0500

    Added the ROS Install Stuff

--- setup.bash
+++ setup.bash
@@ -142,7 +142,18 @@ sudo apt-get intstall emacs vim nmap screen
 echo $COL_CYAN "Getting ROS Indigo for ARM"
 echo $COL_WHITE
 sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
+udo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 
+sudo apt-get update
+sudo apt-get install ros-indigo-ros-base
 
+sudo apt-get install python-rosdep
+sudo rosdep init
+rosdep init
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install python-rosinstall
 
