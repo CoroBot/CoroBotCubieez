c8350f148f826186af959bfdce266f8b8770567c
bleucanary <cowens@coroware.com>
2015-09-03 16:35:55 -0500

    Found a spelling error and resolved it.

--- setup.bash
+++ setup.bash
@@ -142,7 +142,7 @@ sudo apt-get intstall emacs vim nmap screen
 echo $COL_CYAN "Getting ROS Indigo for ARM"
 echo $COL_WHITE
 sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
-udo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 
